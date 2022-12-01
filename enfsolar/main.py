7e7413ecf643dbc7bd62a3c974fa7d7471854ee9
zinchyk.a <a.zinchyk83@gmail.com>
2022-12-01 14:07:50 +0200

    Рабочий парсер

--- enfsolar/main.py
+++ enfsolar/main.py
@@ -32,27 +32,18 @@ driver = webdriver.Chrome(
     service=driver_service,
     options=options
 )
-# Окно браузера на весь экран
-driver.maximize_window()
+
+
+# # Окно браузера на весь экран
+# driver.maximize_window()
 
 
 # Для работы webdriver____________________________________________________
 
 def get_content(url):
-    # Создание файла отчета
-    # with open("soft.csv", "w") as file:
-    #     writer = csv.writer(file, delimiter=";", lineterminator="\r")
-    #     writer.writerow(
-    #         (
-    #             "Ссылка",
-    #             "Просмотры",
-    #             "Город"
-    #         )
-    #     )
     header = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/107.0.0.0 Safari/537.36 "
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"
     }
 
     try:
@@ -68,10 +59,11 @@ def get_content(url):
             for j in country_list:
                 href = 'https://www.enfsolar.com' + j.find_next('a').get("href")
                 url_country.append(href)
-
+        print(url_country)
         for item in url_country[0:1]:
             driver.implicitly_wait(5)
             driver.get(f'{item}')
+            country = item.split("/")[-1]
             # Листать по страницам ---------------------------------------------------------------------------
             isNextDisable = False
             while not isNextDisable:
@@ -90,13 +82,70 @@ def get_content(url):
                     # next_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//i[@class="fa fa-chevron-right"]')))
                     next_button = driver.find_element(By.XPATH, '//i[@class="fa fa-chevron-right"]')
                     # Проверка на наличие кнопки следующая страница, если есть, тогда листаем!
-                    if next_button:
+                    if next_button[0:1]:
                         next_button.click()
                     else:
                         isNextDisable = True
                 except:
                     isNextDisable = True
             # Листать по страницам ---------------------------------------------------------------------------
+        with open("countrys_firma.csv", "w", errors='ignore') as file:
+            writer = csv.writer(file, delimiter=";", lineterminator="\r")
+            writer.writerow(
+                (
+                    'Название фирмы',
+                    'Адрес фирмы',
+                    'Телефон фирмы',
+                    'Email фирмы',
+                    'Сайт фирмы',
+                    'Страна'
+                )
+            )
+        for href_url in url_firma:
+            driver.get(f"{href_url}")
+            try:
+                name_firma = driver.find_element(By.XPATH, '//h1[@itemprop="name"]').text
+            except:
+                name_firma = 'Нет названия фирмы'
+            try:
+                adress_firma = driver.find_element(By.XPATH, '//td[@itemprop="address"]').text
+            except:
+                adress_firma = 'Нет адресса фирмы'
+            try:
+                # Получение информации с атрибута, атрибут можно найти на вкладке Properties
+                tel_firma_ = driver.find_element(By.XPATH, '//td[@itemprop="telephone"]').click()
+                time.sleep(1)
+                tel_firma = driver.find_element(By.XPATH, '//td[@itemprop="telephone"]').get_attribute('outerText')
+            except:
+                tel_firma = 'Нет телефона фирмы'
+            try:
+                email_firma_ = driver.find_element(By.XPATH, '//td[@itemprop="email"]').click()
+                time.sleep(1)
+                email_firma = driver.find_element(By.XPATH, '//td[@itemprop="email"]').get_attribute('outerText')
+            except:
+                email_firma = 'Нет email фирмы'
+            try:
+                www_firma = driver.find_element(By.XPATH, '//a[@itemprop="url"]').get_attribute('outerText')
+            except:
+                www_firma = 'Нет сайта фирмы'
+            try:
+                countrys_firma = driver.find_elements(By.XPATH, '//span[@itemprop="name"]')
+                country_firma = countrys_firma[1].text
+            except:
+                countrys_firma = 'Нет страны фирмы'
+            with open("countrys_firma.csv", "a", errors='ignore') as file:
+                writer = csv.writer(file, delimiter=";", lineterminator="\r")
+                writer.writerow(
+                    (
+                        name_firma,
+                        adress_firma,
+                        tel_firma,
+                        email_firma,
+                        www_firma,
+                        country
+                    )
+                )
+
 
 
     except Exception as ex:
