893293fd5b0f56b9c430812ae52abc04b3726274
zinchyk.a <a.zinchyk83@gmail.com>
2023-01-13 10:10:36 +0200

    13012023

--- Temp/panda.py
+++ Temp/panda.py
@@ -1,32 +1,43 @@
-import time
-
 import pandas as pd
 import glob
 
-# files_xlsx = glob.glob("C:\\scrap_tutorial-master\\uniongroup\panda\\*.xlsx")
-files_csv = glob.glob("C:\\scrap_tutorial-master\\uniongroup\panda\\*.csv")
-
 # Преоброзование Ексель файла в CSV
-# read_file = pd.read_excel("C:\\scrap_tutorial-master\\uniongroup\panda\\price.xlsx")
-# read_file.to_csv("C:\\scrap_tutorial-master\\uniongroup\panda\\price.csv",
+# read_file = pd.read_excel("C:\\scrap_tutorial-master\\uniongroup\\panda\\price_temp.xlsx")
+# read_file.to_csv("C:\\scrap_tutorial-master\\uniongroup\\panda\\price.csv",
 #                  sep=';',
 #                  encoding='cp1251',
 #                  index=None,
 #                  header=True
 #                  )
-#
-combined = pd.DataFrame()
-for file in files_csv:
-    data = pd.read_csv(file,sep=';', encoding='cp1251')
-    # data = pd.read_excel(file)
 
-    print(data)
-# data['filename'] = file.replace(".json", "").split("\\")[-1].replace(" ", "_").replace(".csv", "")
-# combined = pd.concat([combined, data])
+# # объединение нескольких файлов в один___________________________________________________________
+# # Пути к файлам
+# # files_xlsx = glob.glob("C:\\scrap_tutorial-master\\uniongroup\\panda\\*.xlsx")
+# files_csv = glob.glob("C:\\scrap_tutorial-master\\uniongroup\\data\\*.csv")
+# combined = pd.DataFrame()
+# for file in files_csv:
+#     data = pd.read_csv(file,sep=';', encoding='cp1251')
+#     # Добавка значения имени файла в колонку
+#     data['filename'] = file.replace(".json", "").split("\\")[-1].replace(" ", "_").replace(".csv", "")
+#     combined = pd.concat([combined, data])
 #
 # combined.to_csv(
-#     "C:\\scrap_tutorial-master\\uniongroup\panda\\test.csv",
+#     "C:\\scrap_tutorial-master\\uniongroup\\data\\test.csv",
 #     encoding='cp1251',
 #     index=False,
 #     sep=';'
 # )
+# # объединение нескольких файлов в один___________________________________________________________
+
+price_file = ("C:\\scrap_tutorial-master\\uniongroup\\data\\price.csv")
+all_file = ("C:\\scrap_tutorial-master\\uniongroup\\data\\all.csv")
+
+price_dataframe = pd.read_csv(price_file ,sep=';', encoding='cp1251')
+all_dataframe = pd.read_csv(all_file ,sep=';', encoding='cp1251')
+merge_dataframe = all_dataframe.merge(price_dataframe, on=['card_code'])
+merge_dataframe.to_csv(
+    "C:\\scrap_tutorial-master\\uniongroup\\data\\prom.csv",
+    encoding='cp1251',
+    index=False,
+    sep=';'
+)
