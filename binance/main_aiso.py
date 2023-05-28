142cf6969bbed94d755546f8a519c018406f070e
zinchyk.a <a.zinchyk83@gmail.com>
2023-05-28 15:18:20 +0300

    Рабочий

--- binance/main_aiso.py
+++ binance/main_aiso.py
@@ -8,12 +8,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import bot_token, chat_id, PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS
 
-previous_low_price = None
-previous_high_price = None
-
 bot = Bot(token=bot_token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+previous_low_prices = {}
+previous_high_prices = {}
+
 
 async def fetch_data():
     async def read_csv():
@@ -21,6 +22,7 @@ async def fetch_data():
             reader = csv.reader(file, delimiter=' ', quotechar='|')
             urls = list(reader)
         return urls
+
     urls = await read_csv()
 
     async with aiohttp.ClientSession(connector=aiohttp.connector.TCPConnector(ssl=False, limit=None, force_close=True)) as session:
@@ -28,7 +30,8 @@ async def fetch_data():
             futures = url[0]
             url = f'https://www.binance.com/fapi/v1/ticker/24hr?symbol={futures}'
             try:
-                async with session.get(url, proxy=f'http://{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}') as response:
+                async with session.get(url,
+                                       proxy=f'http://{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}') as response:
                     src = await response.text()
             except ClientProxyConnectionError as e:
                 print(f'Failed to connect to the proxy: {PROXY_HOST}:{PROXY_PORT}')
@@ -37,46 +40,39 @@ async def fetch_data():
             soup = BeautifulSoup(src, 'html.parser')
             json_data = soup.string
             data = json.loads(json_data)
-
-            # Проверка наличия ключа 'symbol' в данных JSON
-            if 'symbol' not in data:
-                print(futures)
-                print('*'*100)
-                print(f'Symbol key not found in JSON data: {data}')
-                continue
-
-            # Извлечение значений
             symbol = data['symbol']
             high_price = float(data['highPrice'])
             low_price = float(data['lowPrice'])
             quote_volume = data['quoteVolume']
 
             # Сравнение значений с предыдущими значениями
-            global previous_low_price, previous_high_price
-            if previous_low_price is not None and low_price < previous_low_price:
+            if futures in previous_low_prices and low_price < previous_low_prices[futures]:
                 print(f'{symbol} минимальная: {low_price}')
                 message = f'{symbol} минимальная: {low_price}'
                 await send_message_to_group(message)
 
-            if previous_high_price is not None and high_price > previous_high_price:
+            if futures in previous_high_prices and high_price > previous_high_prices[futures]:
                 print(f'{symbol} максимальная: {high_price}')
                 message = f'{symbol} максимальная: {high_price}'
                 await send_message_to_group(message)
 
             # Обновление предыдущих значений
-            previous_low_price = low_price
-            previous_high_price = high_price
+            previous_low_prices[futures] = low_price
+            previous_high_prices[futures] = high_price
 
     # Закрытие сессии и коннектора
     await session.close()
 
+
 async def send_message_to_group(message):
     await bot.send_message(chat_id=chat_id, text=message)
+
 
 async def main():
     while True:
         await fetch_data()
         await asyncio.sleep(3)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
