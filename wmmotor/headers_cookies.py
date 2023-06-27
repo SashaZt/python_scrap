2a0fcccda8383c969021daa367bc43301ac62754
zinchyk.a <a.zinchyk83@gmail.com>
2023-06-27 10:54:00 +0300

    270623

---
+++ wmmotor/headers_cookies.py
@@ -0,0 +1,19 @@
+cookies = {
+    'weblogin[name]': 'kupujwpl%40gmail.com',
+    'weblogin[save]': '1',
+    'listconfig': 'YTo4OntzOjc6InNob3dhbGwiO2k6MTtzOjg6InNob3dwaWNzIjtpOjE7czo5OiJzaG93cHJpY2UiO2k6MTtzOjk6InByaWNldHlwZSI7aTowO3M6OToicHJvbW90eXBlIjtpOjA7czo5OiJsaXN0Y291bnQiO3M6MjoiNTAiO3M6ODoibGlzdHNvcnQiO2k6MDtzOjc6Imxpc3RkaXIiO2k6MDt9',
+    'sid': '280b30b669416d18225150e26608d3dd',
+    'cookies_accepted': 'T',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'weblogin[name]=kupujwpl%40gmail.com; weblogin[save]=1; listconfig=YTo4OntzOjc6InNob3dhbGwiO2k6MTtzOjg6InNob3dwaWNzIjtpOjE7czo5OiJzaG93cHJpY2UiO2k6MTtzOjk6InByaWNldHlwZSI7aTowO3M6OToicHJvbW90eXBlIjtpOjA7czo5OiJsaXN0Y291bnQiO3M6MjoiNTAiO3M6ODoibGlzdHNvcnQiO2k6MDtzOjc6Imxpc3RkaXIiO2k6MDt9; sid=280b30b669416d18225150e26608d3dd; cookies_accepted=T',
+    'DNT': '1',
+    'Referer': 'http://www.wmmotor.pl/hurtownia/towar.php?a=1&idtow=68452&grupa=MCwxMDAy&back=c3p1a2FqLnBocD9zenVrYWo9QVdSOTIxMiZwYWdlPTE%3D',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+}
