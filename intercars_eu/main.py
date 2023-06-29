c774c31ca7ff1e06fe7500d6cb5720afc95ac829
zinchyk.a <a.zinchyk83@gmail.com>
2023-06-29 13:45:31 +0300

    Тестирование

---
+++ intercars_eu/main.py
@@ -0,0 +1,148 @@
+import os
+import shutil
+import tempfile
+import zipfile
+import time
+import random
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from concurrent.futures import ThreadPoolExecutor
+import csv
+from selenium.webdriver.common.keys import Keys
+
+
+class ProxyExtension:
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {"scripts": ["background.js"]},
+        "minimum_chrome_version": "76.0.0"
+    }
+    """
+
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %d
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        { urls: ["<all_urls>"] },
+        ['blocking']
+    );
+    """
+
+    def __init__(self, host, port, user, password):
+        self._dir = os.path.normpath(tempfile.mkdtemp())
+
+        manifest_file = os.path.join(self._dir, "manifest.json")
+        with open(manifest_file, mode="w") as f:
+            f.write(self.manifest_json)
+
+        background_js = self.background_js % (host, port, user, password)
+        background_file = os.path.join(self._dir, "background.js")
+        with open(background_file, mode="w") as f:
+            f.write(background_js)
+
+    @property
+    def directory(self):
+        return self._dir
+
+    def __del__(self):
+        shutil.rmtree(self._dir)
+
+
+def get_chromedriver():
+    chrome_options = webdriver.ChromeOptions()
+
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    # chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    # chrome_options.add_argument('--disable-extensions') # Отключает использование расширений
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-setuid-sandbox')
+    chrome_options.add_argument(
+        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36')
+    # proxy_extension = ProxyExtension(*proxy)
+    # chrome_options.add_argument(f"--load-extension={proxy_extension.directory}")
+    s = Service(executable_path="C:\\scrap_tutorial-master\\chromedriver.exe")
+    driver = webdriver.Chrome(service=s, options=chrome_options)
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        'source': '''
+            delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
+            delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+            delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+      '''
+    })
+    return driver
+
+
+def get_selenium():
+    url = 'https://ua.e-cat.intercars.eu/ru/motorcycles'
+    driver = get_chromedriver()
+    driver.maximize_window()
+    driver.get(url)
+    try:
+        wait = WebDriverWait(driver, 60)
+        wait_email = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//input[@id="loginForm:username"]')))
+        email = driver.find_element(By.XPATH, '//input[@id="loginForm:username"]')
+        email.send_keys('logisticamotoprox@gmail.com')
+        passwords = driver.find_element(By.XPATH, '//input[@id="loginForm:password"]')
+        passwords.send_keys('GEkz54x!')
+        passwords.send_keys(Keys.RETURN)
+        wait_category = wait.until(
+            EC.presence_of_element_located((By.XPATH, '//div[@class="yCmsContentSlot"]')))
+        time.sleep(1)
+        file_name = f"motorcycles.html"
+        with open(file_name, "w", encoding='utf-8') as fl:
+            fl.write(driver.page_source)
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+
+if __name__ == '__main__':
+    get_selenium()
