4bd870955014b7a82aca72adcadff144e630fc89
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-02 22:50:34 +0300

    Добавил cf_clearance

--- Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
+++ Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
@@ -6,6 +6,8 @@ import requests
 import json
 import cloudscraper
 import os
+from playwright.sync_api import sync_playwright
+from cf_clearance import sync_cf_retry, sync_stealth
 import time
 import shutil
 import tempfile
@@ -217,6 +219,39 @@ def get_selenium():
     file_name = f"AMELIBOOST.html"
     with open(os.path.join('data', file_name), "w", encoding='utf-8') as fl:
         fl.write(driver.page_source)
+
+def get_coocies():
+    """Получение куки из любого сайта и отправка их в reqest"""
+    url = 'https://www.vaurioajoneuvo.fi/?model_year_min=1999'
+        # not use cf_clearance, cf challenge is fail
+        # proxies = {
+        #     "all": "socks5://localhost:7890"
+        # }
+    res = requests.get(url)
+    if '<title>Just a moment...</title>' in res.text:
+        print("cf challenge fail")
+    # get cf_clearance
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        sync_stealth(page, pure=True)
+        page.goto(url)
+        res = sync_cf_retry(page)
+        if res:
+            cookies = page.context.cookies()
+            for cookie in cookies:
+                if cookie.get('name') == 'cf_clearance':
+                    cf_clearance_value = cookie.get('value')
+                    # print(cf_clearance_value)
+            ua = page.evaluate('() => {return navigator.userAgent}')
+            # print(ua)
+            # get_cloudscraper(ua, cf_clearance_value)
+
+        else:
+            print("cf challenge fail")
+
+        browser.close()
+        # return ua, cf_clearance_value  # Возвращаем значения
 
 if __name__ == '__main__':
     # get_requests()
