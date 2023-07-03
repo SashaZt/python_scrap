53688553d743017c604e4d15909ce089dada2db6
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-03 14:00:45 +0300

    Все работает

--- tripoli_land/main.py
+++ tripoli_land/main.py
@@ -64,44 +64,180 @@ def get_selenium():
     passwords.send_keys('Stepan9472')
     passwords.send_keys(Keys.RETURN)
     time.sleep(1)
-    coun = 480
-    for i in range(13, 22):
+    coun = 0
+    with open('Ровно.csv', 'a', newline='', encoding='utf-8') as file_data:
+        writer = csv.writer(file_data, delimiter=';')
+        writer.writerow(['', '', '', '', '', '', '', ''])
+        for i in range(1, 13):
 
-        pause_time = random.randint(1, 5)
-        driver.get(
-            f'https://tripoli.land/farmers/proizvoditeli-zerna/ivano-frankovskaya?page={i}&q%5Bcategories_id_eq%5D=2&q%5Bdistrict_region_id_eq%5D=9')
-        wait_company = wait.until(
-            EC.presence_of_element_located((By.XPATH, '//span[@class="call-popup"]')))
-        company = driver.find_element(By.XPATH, '//span[@class="call-popup"]')
-        company.click()
-        for j in range(1, 41):
-            coun += 1
-            file_name = f"c:\\data_tripoli_land\\farmers_{coun}.html"
-            # if not os.path.exists(file_name):
-            time.sleep(1)
-            # driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-            with open(os.path.join('data', file_name), "w", encoding='utf-8') as fl:
-                fl.write(driver.page_source)
+            pause_time = random.randint(1, 5)
+            driver.get(
+                f'https://tripoli.land/farmers/proizvoditeli-zerna/rovenskaya?page={i}&q%5Bcategories_id_eq%5D=2&q%5Bdistrict_region_id_eq%5D=18')
+            wait_company = wait.until(
+                EC.presence_of_element_located((By.XPATH, '//span[@class="call-popup"]')))
+            company = driver.find_element(By.XPATH, '//span[@class="call-popup"]')
+            company.click()
+            for j in range(1, 41):
+                coun += 1
+                # file_name = f"c:\\data_tripoli_land\\farmers_{coun}.html"
+                # if not os.path.exists(file_name):
+                time.sleep(1)
+                # driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+                # with open(os.path.join('data', file_name), "w", encoding='utf-8') as fl:
+                #     fl.write(driver.page_source)
                 time.sleep(pause_time)
-            company_nex = driver.find_element(By.XPATH,
-                                              '//button[@class="non-fluid right modal-action-btn tips-tooltip ng-scope"]')
-            company_nex.click()
-        # clouse_button = driver.find_element(By.XPATH, '//div[@class="s-close-btn"]')
-        # clouse_button.click()
+                company_nex = driver.find_element(By.XPATH,
+                                                  '//button[@class="non-fluid right modal-action-btn tips-tooltip ng-scope"]')
+                soup = BeautifulSoup(driver.page_source, 'lxml')
+                regex_all = re.compile('beige-hover ng-scope.*')
+                telephoni_company = soup.find('table', attrs={'ng-if': '!isMobile()'}).find_all('tbody')[0].find_all(
+                    'tr', attrs={'class': regex_all})
+                contacts = []
+                for i in range(0, len(telephoni_company)):
+                    regex_one = re.compile('same-phone-widt.*')
+                    try:
 
-    driver.close()
-    driver.quit()
+                        telef = telephoni_company[i].find('a', attrs={'class': regex_one}).get('ng-href')
+                    except AttributeError:
+                        telef = ""
+                    try:
+                        names = telephoni_company[i].find('span', attrs={
+                            'class': 'phone-comment phone-comment-edit-pencil tablink-correct tablink-correct hide-hover tips-tooltip ng-binding ng-scope'}).text.strip()
+                    except AttributeError:
+                        names = ""
+                    try:
+                        job = telephoni_company[i].find('span', attrs={
+                            'class': 'dropdown-toggle position-title tips-tooltip ng-binding'}).text.strip()
+                    except AttributeError:
+                        job = ""
+                    if not telef == "":
+                        contacts.append(telef)
+                    if not names == "":
+                        contacts.append(names)
+                    if not job == "":
+                        contacts.append(job)
+                contacts_email = []
+
+                regex_email = re.compile('beige-hover ng-scope.*')
+                try:
+                    tables_email = soup.find_all('table', {'class': 'web-phones ng-scope'})
+                    # email_company = soup.find_all('table', attrs={'class': 'web-phones ng-scope'})[1].find_all('tbody')[0].find_all(
+                    #     'tr', attrs={'class': regex_email})
+                except:
+                    tables_email = None
+                    # email_company = None
+                try:
+                    table = tables_email[1]
+
+                except:
+                    table = None
+                if not table is None:
+                    rows = table.find_all('tr')
+                    for row in rows:
+                        # Ищем теги 'span' с классом 'ng-binding'
+                        spans = row.find_all('span', {'class': 'ng-binding'})
+                        for span in spans:
+                            # Извлекаем текст из каждого 'span' и добавляем в список
+                            contacts_email.append(span.text)
+
+                #
+                # for i in range(0, len(email_company)):
+                #     regex_one = re.compile('same-phone-widt.*')
+                #     try:
+                #
+                #         email = email_company[i].find('span', attrs={'class': 'ng-binding'}).text
+                #     except AttributeError:
+                #         email = ""
+                #     # try:
+                #     #     names = email_company[i].find('span', attrs={
+                #     #         'class': 'phone-comment phone-comment-edit-pencil tablink-correct tablink-correct hide-hover tips-tooltip ng-binding ng-scope'}).text.strip()
+                #     # except AttributeError:
+                #     #     names = ""
+                #     # try:
+                #     #     job = email_company[i].find('span', attrs={
+                #     #         'class': 'dropdown-toggle position-title tips-tooltip ng-binding'}).text.strip()
+                #     # except AttributeError:
+                #     #     job = ""
+                #     if not email == "":
+                #         contacts_email.append(email)
+                    # if not names == "":
+                    #     contacts.append(names)
+                    # if not job == "":
+                    #     contacts.append(job)
+                # print(contacts_email)
+
+                edrpo = soup.find('div', attrs={'ng-if': 'org.erdpou'}).find('div',
+                                                                             attrs={'class': 'ng-binding'}).text
+                address = soup.find('div', attrs={'ng-bind': 'org.address_label'}).text.split(',')
+                try:
+                    address_company_01 = address[0]
+                except:
+                    address_company_01 = ""
+                try:
+                    address_company_02 = address[1]
+                except:
+                    address_company_02 = ""
+                try:
+                    address_company_03 = address[2]
+                except:
+                    address_company_03 = ""
+                try:
+                    address_company_04 = address[3]
+                except:
+                    address_company_04 = ""
+                try:
+                    address_company_05 = address[4]
+                except:
+                    address_company_05 = ""
+                try:
+                    address_company_06 = address[5]
+                except:
+                    address_company_06 = ""
+                try:
+                    address_company_07 = address[6]
+                except:
+                    address_company_07 = ""
+                try:
+                    address_company_08 = address[7]
+                except:
+                    address_company_08 = ""
+                name_company = soup.find('p', attrs={
+                    'class': 'modal-title display-inline-block farmer-modal-name ng-binding'}).text
+                try:
+                    landing_area_sum = soup.find('div',
+                                                 attrs={'ng-bind': 'org.landing_area_sum | splitThousands'}).text
+                except:
+                    landing_area_sum = ""
+                try:
+                    director = soup.find('div', attrs={'ng-bind': 'org.director'}).text
+                except:
+                    director = ""
+                datas = [address_company_01, address_company_02, address_company_03, address_company_04,
+                         address_company_05, address_company_06, address_company_07, address_company_08,
+                         name_company, edrpo, director, contacts, contacts_email, landing_area_sum
+                         ]
+                writer.writerow(
+                    datas
+                )
+
+                company_nex.click()
+            # clouse_button = driver.find_element(By.XPATH, '//div[@class="s-close-btn"]')
+            # clouse_button.click()
+
+        driver.close()
+        driver.quit()
 
 
 def parsing():
-    files_htmls = f"c:\\data_tripoli_land\\Тернопольская\\*.html"
+    files_htmls = f"c:\\data_tripoli_land\\Льовськая\\*.html"
     files_html = glob.glob(files_htmls)
 
-    with open('csv_01.csv', 'w', newline='', encoding='utf-8') as file_data:
+    with open('Ровно.csv', 'w', newline='', encoding='utf-8') as file_data:
         writer = csv.writer(file_data, delimiter=';')
         writer.writerow(['', '', '', '', '', '', ''])
 
-        for item in files_html:
+        for item in files_html[150:160]:
+            print(item)
             with open(item, encoding="utf-8") as file:
                 src = file.read()
             soup = BeautifulSoup(src, 'lxml')
@@ -169,7 +305,8 @@ def parsing():
                 address_company_08 = address[7]
             except:
                 address_company_08 = ""
-            name_company = soup.find('p', attrs={'class': 'modal-title display-inline-block farmer-modal-name ng-binding'}).text
+            name_company = soup.find('p', attrs={
+                'class': 'modal-title display-inline-block farmer-modal-name ng-binding'}).text
             try:
                 landing_area_sum = soup.find('div', attrs={'ng-bind': 'org.landing_area_sum | splitThousands'}).text
             except:
