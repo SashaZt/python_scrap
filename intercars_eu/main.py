af1decadb4d171134407633fc19cb8e27082a40d
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-05 13:19:11 +0300

    Рабочий

--- intercars_eu/main.py
+++ intercars_eu/main.py
@@ -117,18 +117,21 @@ def get_chromedriver():
       '''
     })
     return driver
+
+
 def get_chromedriver_parsing():
     chrome_options = webdriver.ChromeOptions()
 
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-extensions') # Отключает использование расширений
+    chrome_options.add_argument('--disable-extensions')  # Отключает использование расширений
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-setuid-sandbox')
     s = Service(executable_path="C:\\scrap_tutorial-master\\chromedriver.exe")
     driver = webdriver.Chrome(service=s, options=chrome_options)
     return driver
+
 
 def get_category():
     cookies = {
@@ -181,15 +184,15 @@ def get_category():
 
 def get_product():
     urls = [
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Drive/Motorcycle-drive/Chain-Belt/c/tecdoc-6500000-5010888-6515040?q=%3Adefault%3AbranchAvailability%3AALL%3Amarket%3AMOT",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Batteries-and-cranking-system/c/tecdoc-5090003?q=%3Adefault%3Aicgoods_35545%3Aall_markets%3AbranchAvailability%3AALL%3Amarket%3AMOT",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Brake-system/Disc-brakes/Brake-pad/c/tecdoc-6600000-6611000-6611010?q=:default:market:MOT",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Oils-Greases-Motorization-chemistry/c/tecdoc-5090009?q=%3adefault:icgoods_38683:icgoods_366384:icgoods_25999:icgoods_289274",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Vehicle-shock-absorption/Shock-absorbers/Shock-Absorber-Oil/c/tecdoc-6400000-6411000-genart_9563?q=:default",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Oils-Greases-Motorization-chemistry/c/tecdoc-5090009?q=%3adefault:icgoods_38683:icgoods_366384:icgoods_25999:icgoods_289280",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Oils-Greases-Motorization-chemistry/Greases/Special-grease/Chain-Spray/c/tecdoc-5090009-5010183-5010190-genart_3201?q=:default",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Cooling-system/Antifreeze/c/tecdoc-6900000-6923000?q=:default:branchAvailability:ALL:market:MOT",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Oils-Greases-Motorization-chemistry/Brake-fluids/c/tecdoc-5090009-5010195?q=%3Adefault%3AbranchAvailability%3AALL",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9F%D1%80%D0%B8%D0%B2%D0%BE%D0%B4/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D0%B5%D1%82%D0%BD%D0%B8%D0%B9-%D0%BF%D1%80%D0%B8%D0%B2%D1%96%D0%B4/%D0%A6%D0%B5%D0%BF%D1%8C-%D0%A0%D0%B5%D0%BC%D0%B5%D0%BD%D1%8C/c/tecdoc-6500000-5010888-6515040?q=%3Adefault%3AbranchAvailability%3AALL%3Amarket%3AMOT",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%90%D0%BA%D0%BA%D1%83%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%D1%8B-%D0%B8-%D0%B7%D0%B0%D0%BF%D1%83%D1%81%D0%BA-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8F/c/tecdoc-5090003?q=%3Adefault%3Aicgoods_35545%3Aall_markets%3AbranchAvailability%3AALL%3Amarket%3AMOT",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%A2%D0%BE%D1%80%D0%BC%D0%BE%D0%B7%D0%BD%D0%B0%D1%8F-%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0/%D0%94%D0%B8%D1%81%D0%BA%D0%BE%D0%B2%D1%8B%D0%B9-%D1%82%D0%BE%D1%80%D0%BC%D0%BE%D0%B7/%D0%A2%D0%BE%D1%80%D0%BC%D0%BE%D0%B7%D0%BD%D0%B0%D1%8F-%D0%BA%D0%BE%D0%BB%D0%BE%D0%B4%D0%BA%D0%B0-%D0%B4%D0%B8%D1%81%D0%BA%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE-%D1%82%D0%BE%D1%80%D0%BC%D0%BE%D0%B7%D0%B0/c/tecdoc-6600000-6611000-6611010?q=:default:market:MOT",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%B0%D1%81%D0%BB%D0%B0-%D0%A1%D0%BC%D0%B0%D0%B7%D0%BA%D0%B8-%D0%9C%D0%BE%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%85%D0%B8%D0%BC%D0%B8%D1%8F/c/tecdoc-5090009?q=%3adefault:icgoods_38683:icgoods_366384:icgoods_25999:icgoods_289274",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%90%D0%BC%D0%BE%D1%80%D1%82%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-%D0%B0%D0%B2%D1%82%D0%BE%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%BE%D0%B3%D0%BE-%D1%81%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2a/%D0%90%D0%BC%D0%BE%D1%80%D1%82%D0%B8%D0%B7%D0%B0%D1%82%D0%BE%D1%80%D1%8B/%D0%90%D0%BC%D0%BE%D1%80%D1%82%D0%B8%D0%B7%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F-%D0%B6%D0%B8%D0%B4%D0%BA%D0%BE%D1%81%D1%82%D1%8C/c/tecdoc-6400000-6411000-genart_9563?q=:default",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%B0%D1%81%D0%BB%D0%B0-%D0%A1%D0%BC%D0%B0%D0%B7%D0%BA%D0%B8-%D0%9C%D0%BE%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%85%D0%B8%D0%BC%D0%B8%D1%8F/c/tecdoc-5090009?q=%3adefault:icgoods_38683:icgoods_366384:icgoods_25999:icgoods_289280",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%B0%D1%81%D0%BB%D0%B0-%D0%A1%D0%BC%D0%B0%D0%B7%D0%BA%D0%B8-%D0%9C%D0%BE%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%85%D0%B8%D0%BC%D0%B8%D1%8F/%D0%A1%D0%BC%D0%B0%D0%B7%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F-%D1%81%D0%BC%D0%B0%D0%B7%D0%BA%D0%B0/%D0%96%D0%B8%D0%B4%D0%BA%D0%BE%D1%81%D1%82%D1%8C-%D0%B4%D0%BB%D1%8F-%D1%86%D0%B5%D0%BF%D0%B8/c/tecdoc-5090009-5010183-5010190-genart_3201?q=:default",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9E%D1%85%D0%BB%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5/%D0%90%D0%BD%D1%82%D0%B8%D1%84%D1%80%D0%B8%D0%B7/c/tecdoc-6900000-6923000?q=:default:branchAvailability:ALL:market:MOT",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%B0%D1%81%D0%BB%D0%B0-%D0%A1%D0%BC%D0%B0%D0%B7%D0%BA%D0%B8-%D0%9C%D0%BE%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%85%D0%B8%D0%BC%D0%B8%D1%8F/%D0%A2%D0%BE%D1%80%D0%BC%D0%BE%D0%B7%D0%BD%D1%8B%D0%B5-%D0%B6%D0%B8%D0%B4%D0%BA%D0%BE%D1%81%D1%82%D0%B8/c/tecdoc-5090009-5010195?q=%3Adefault%3AbranchAvailability%3AALL",
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%9E%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0-%D0%B8-%D0%B0%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%9A%D0%BE%D0%BC%D0%B1%D0%B8%D0%BD%D0%B5%D0%B7%D0%BE%D0%BD%D1%8B/c/tecdoc-5090012-5010763-5010753",
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%9E%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0-%D0%B8-%D0%B0%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%92%D0%BD%D0%B5%D0%B4%D0%BE%D1%80%D0%BE%D0%B6%D0%BD%D1%8B%D0%B9/%D0%A4%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%B1%D0%B5%D0%B7%D0%B4%D0%BE%D1%80%D0%BE%D0%B6%D1%8C%D1%8F/c/tecdoc-5090012-5010763-5010771-genart_92452",
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%9E%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0-%D0%B8-%D0%B0%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%9A%D1%83%D1%80%D1%82%D0%BA%D0%B8/c/tecdoc-5090012-5010763-5010750",
@@ -200,10 +203,10 @@ def get_product():
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%A8%D0%BB%D0%B5%D0%BC%D1%8B/c/tecdoc-5090012-5010749",
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%A8%D0%BB%D0%B5%D0%BC%D1%8B/%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B-%D0%B4%D0%BB%D1%8F-%D1%88%D0%BB%D0%B5%D0%BC%D0%B0/c/tecdoc-5090012-5010749-genart_92440",
         "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%9C%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D0%BE%D0%B2/%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D0%BE%D0%B2/%D0%9F%D1%83%D1%82%D0%B5%D1%88%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D0%B5-%D0%B8-%D0%B1%D0%B0%D0%B3%D0%B0%D0%B6/c/tecdoc-5090012-5010858-5010515-genart_92446",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Tyres-Wheels-Accessories/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1028756",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Tyres-Wheels-Accessories/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056313",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Tyres-Wheels-Accessories/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056314",
-        "https://ua.e-cat.intercars.eu/ru/Full-offer/Tyres-Wheels-Accessories/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056315"
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%A8%D0%B8%D0%BD%D1%8B-%D0%9A%D0%BE%D0%BB%D1%91%D1%81%D0%B0-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1028756",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%A8%D0%B8%D0%BD%D1%8B-%D0%9A%D0%BE%D0%BB%D1%91%D1%81%D0%B0-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056313",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%A8%D0%B8%D0%BD%D1%8B-%D0%9A%D0%BE%D0%BB%D1%91%D1%81%D0%B0-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056314",
+        "https://ua.e-cat.intercars.eu/ru/%D0%92%D1%81%D0%B5-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D0%B8/%D0%A8%D0%B8%D0%BD%D1%8B-%D0%9A%D0%BE%D0%BB%D1%91%D1%81%D0%B0-%D0%90%D0%BA%D1%81%D0%B5%D1%81%D1%81%D1%83%D0%B0%D1%80%D1%8B/c/tecdoc-5090008?q=:default:icgoods_63966:icgoods_1028752:branchAvailability:ALL:icgoods_64319:icgoods_1056315"
     ]
     cookies = {
         'BTID': '4C54089E-29F3-4B81-9FFE-47F1678DF42D',
@@ -305,9 +308,10 @@ def parsing():
         writer = csv.writer(csvfile, delimiter=";")
         for i in files_html:
             driver.get(i)
-            coun +=1
-            category = driver.find_element(By.XPATH, '//a[@data-test="parentCategoryName"]').get_attribute("title").replace("/",
-                                                                                                                            "_")
+            coun += 1
+            category = driver.find_element(By.XPATH, '//a[@data-test="parentCategoryName"]').get_attribute(
+                "title").replace("/",
+                                 "_")
             table_product = driver.find_element(By.XPATH, '//table[@class="listingcollapsed__wrapper"]')
             products = table_product.find_elements(By.XPATH, "//tbody[contains(@class, 'listingcollapsed__item')]")
             for i in products:
