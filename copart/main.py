45806a246af8e8d29de6ac1cdf50fb7b715916b0
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-05 13:00:47 +0300

    Тестирую

--- copart/main.py
+++ copart/main.py
@@ -238,6 +238,75 @@ def get_id_ad_and_url():
         # print(content)
 
 def get_product():
+    cookies = {
+        'userLang': 'en',
+        'nlbi_242093': '3VItFF2VOBoHUUB+JDHybgAAAABsraBHQ9k9WpA8YOPY8dxS',
+        'timezone': 'Europe%2FKiev',
+        's_ppv': '100',
+        'userCategory': 'RPU',
+        's_fid': '7B97EB489ACC0C92-3446E08462F183D3',
+        'g2usersessionid': '0b4f61da6613900ecce840bc5d774668',
+        'search-table-rows': '20',
+        'visid_incap_242093': 'UOYi1RRfTzyJ7jo9JOPVyJz6omQAAAAAQkIPAAAAAACAF2utAZJ4uQRFQhFj8qgTt+ndHEwc+Hdu',
+        'incap_ses_323_242093': 'aIGEbkfOcxhzexi0wId7BJkrpWQAAAAAOM24/pM9WsAdU7V1mu9xFQ==',
+        'G2JSESSIONID': 'DE6C9C9F61E8ED4898A810D7852B0B7B-n1',
+        'classicSearchResultsView': 'true',
+        'reese84': '3:BnX8RLS4MJQAKxRpmEiwew==:a/Xy1nKWB3+dJVmLWI9/dKkHI1h8Qm4LtK8K71kFiBPmWRc8NR2JqWWUyIpn51d8ZLQZWYzwanKvamK5HAqBamrBIiViE9Z6CkIAShngiOOLGWBAyZxjrQhyejpXZavy+5pKAhTKdQc9FM+CrBqi3ag5nHJiuj8MiSCMWEyQ9xa2oCMXiuaXVYpjpXJsOqicLTEdHs0Jz5GojTl/BKudeLdhI2UMCL2zKtpSjZftUfPPTEmmT4B/COFqxvbOynFZATBPOGenWRkdPBGGF5Fvyszdfin5NnJIKD5oIuQ7UkXqtU7VE0PFCWsbyfiSS+BLtUudPxGcWusy1KDwdza6clRzhkpBLrm8DUknE2t4CkAxBnw6J6tApEeY4uQPes7EqOwb/Z2pJeIAX3/ISJi6bRihja5PYeXEniZO3m911kY89NZ0yK1IxZEipI/9SBS5SCLntrQzudNb7mya3dLaS0QRfHPqbBXSp14UFpwVUBd9yOOFGguR6HmHrK9N2/ywPE67IHsU2pECaNKXJaEu5w==:cVJCouWbe77w+B3nL5fwU+fk5hhX7nR5SzEeuoH9/3k=',
+        's_depth': '1',
+        's_pv': 'no%20value',
+        's_nr': '1688549546977-Repeat',
+        's_vnum': '1690995053533%26vn%3D5',
+        's_invisit': 'true',
+        's_lv': '1688549546978',
+        's_lv_s': 'Less%20than%201%20day',
+        'copartTimezonePref': '%7B%22displayStr%22%3A%22GMT%2B3%22%2C%22offset%22%3A3%2C%22dst%22%3Atrue%2C%22windowsTz%22%3A%22Europe%2FKiev%22%7D',
+        'nlbi_242093_2147483392': 'Jai4SSOlUV/hmPIOJDHybgAAAABEl2Q8SU82VAIBNfgoSSQN',
+    }
+
+    headers = {
+        'authority': 'www.copart.com',
+        'accept': '*/*',
+        'accept-language': 'ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6',
+        'cache-control': 'no-cache',
+        # 'cookie': 'userLang=en; nlbi_242093=3VItFF2VOBoHUUB+JDHybgAAAABsraBHQ9k9WpA8YOPY8dxS; timezone=Europe%2FKiev; s_ppv=100; userCategory=RPU; s_fid=7B97EB489ACC0C92-3446E08462F183D3; g2usersessionid=0b4f61da6613900ecce840bc5d774668; search-table-rows=20; visid_incap_242093=UOYi1RRfTzyJ7jo9JOPVyJz6omQAAAAAQkIPAAAAAACAF2utAZJ4uQRFQhFj8qgTt+ndHEwc+Hdu; incap_ses_323_242093=aIGEbkfOcxhzexi0wId7BJkrpWQAAAAAOM24/pM9WsAdU7V1mu9xFQ==; G2JSESSIONID=DE6C9C9F61E8ED4898A810D7852B0B7B-n1; classicSearchResultsView=true; reese84=3:BnX8RLS4MJQAKxRpmEiwew==:a/Xy1nKWB3+dJVmLWI9/dKkHI1h8Qm4LtK8K71kFiBPmWRc8NR2JqWWUyIpn51d8ZLQZWYzwanKvamK5HAqBamrBIiViE9Z6CkIAShngiOOLGWBAyZxjrQhyejpXZavy+5pKAhTKdQc9FM+CrBqi3ag5nHJiuj8MiSCMWEyQ9xa2oCMXiuaXVYpjpXJsOqicLTEdHs0Jz5GojTl/BKudeLdhI2UMCL2zKtpSjZftUfPPTEmmT4B/COFqxvbOynFZATBPOGenWRkdPBGGF5Fvyszdfin5NnJIKD5oIuQ7UkXqtU7VE0PFCWsbyfiSS+BLtUudPxGcWusy1KDwdza6clRzhkpBLrm8DUknE2t4CkAxBnw6J6tApEeY4uQPes7EqOwb/Z2pJeIAX3/ISJi6bRihja5PYeXEniZO3m911kY89NZ0yK1IxZEipI/9SBS5SCLntrQzudNb7mya3dLaS0QRfHPqbBXSp14UFpwVUBd9yOOFGguR6HmHrK9N2/ywPE67IHsU2pECaNKXJaEu5w==:cVJCouWbe77w+B3nL5fwU+fk5hhX7nR5SzEeuoH9/3k=; s_depth=1; s_pv=no%20value; s_nr=1688549546977-Repeat; s_vnum=1690995053533%26vn%3D5; s_invisit=true; s_lv=1688549546978; s_lv_s=Less%20than%201%20day; copartTimezonePref=%7B%22displayStr%22%3A%22GMT%2B3%22%2C%22offset%22%3A3%2C%22dst%22%3Atrue%2C%22windowsTz%22%3A%22Europe%2FKiev%22%7D; nlbi_242093_2147483392=Jai4SSOlUV/hmPIOJDHybgAAAABEl2Q8SU82VAIBNfgoSSQN',
+        'dnt': '1',
+        'pragma': 'no-cache',
+        'referer': 'https://www.copart.com/',
+        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'script',
+        'sec-fetch-mode': 'no-cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    }
+    response = requests.get(
+        'https://www.copart.com/public/data/lotdetails/solr/43079133',
+
+        cookies=cookies,
+        headers=headers, )
+    data = response.json()
+    filename = f"c:\\DATA\\copart\\product\\data.json"
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+def parsin():
+    with open('c:\\DATA\\copart\\product\\data.json', 'r') as f:
+        # Загрузить JSON из файла
+        data = json.load(f)
+    'https: // www.copart.com / lot / 43079133 / salvage - 2018 - ford - focus - se - fl - miami - south'
+    ln = data['data']['lotDetails']['ln']
+    url_lot = f"https://www.copart.com/lot/{ln}"
+    url_img = data['data']['lotDetails']['tims']
+    name_lot = data['data']['lotDetails']['ld']
+    price_bnp = data['data']['lotDetails']['bnp']
+    odometer_lot = data['data']['lotDetails']['orr']
+    drive_lot = data['data']['lotDetails']['drv']
+    engine_type_lot = data['data']['lotDetails']['egn']
+    vehicle_type_lot = data['data']['lotDetails']['vehTypDesc']
+
+
+    print(vehicle_type_lot)
 
 
 if __name__ == '__main__':
@@ -245,4 +314,5 @@ if __name__ == '__main__':
     # get_cloudscraper()
     # get_selenium()
     # get_id_ad_and_url()
-    get_product()
+    # get_product()
+    parsin()
