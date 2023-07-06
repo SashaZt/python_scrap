73f3f0ab3d08e0378a16761e64e8b9d6f24bd8d7
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-06 14:58:02 +0300

    Перед изменением, backup

--- vaurioajoneuvo/main_asio.py
+++ vaurioajoneuvo/main_asio.py
@@ -58,67 +58,72 @@ def process_urls_and_send_messages(bot_token, chat_id, list_urls):
         browser.close()
         # return ua, cf_clearance_value  # Возвращаем значения
     while True:
-            for url in list_urls:
-                # print(url)
-                headers = {"user-agent": ua}
-                cookies = {"cf_clearance": cf_clearance_value}
-                parsed_url = urlparse(url)
-                params = parse_qs(parsed_url.query)
-                params = {k: v[0] for k, v in params.items()}
-                scraper = cloudscraper.create_scraper(browser={
-                    'browser': 'chrome',
-                    'platform': 'windows',
-                    'desktop': True,
-                    'mobile': False})
-                response = scraper.get(url, cookies=cookies, headers=headers)  # , proxies=proxies
-                src = response.text
-                # html = response.content
-                # filename = f"amazon_{coun}.html"
-                # with open(filename, "w", encoding='utf-8') as f:
-                #     f.write(html.decode('utf-8'))
-                soup = BeautifulSoup(src, 'lxml')
-                table_row = soup.find('div', attrs={'class': 'cars-list'})
-                regex_containr = re.compile('.*(?=item-lift-container)')
+        for url in list_urls:
+            # print(url)
+            headers = {"user-agent": ua}
+            cookies = {"cf_clearance": cf_clearance_value}
+            # parsed_url = urlparse(url)
+            # params = parse_qs(parsed_url.query)
+            # params = {k: v[0] for k, v in params.items()}
+            scraper = cloudscraper.create_scraper(browser={
+                'browser': 'chrome',
+                'platform': 'windows',
+                'desktop': True,
+                'mobile': False})
+            # response = scraper.get(url, cookies=cookies, headers=headers)  # , proxies=proxies
+            response = requests.get(url, cookies=cookies, headers=headers)
+            src = response.text
+            print(response.status_code)
+            # html = response.content
+            # filename = f"amazon_.html"
+            # with open(filename, "w", encoding='utf-8') as f:
+            #     f.write(html.decode('utf-8'))
+            soup = BeautifulSoup(src, 'lxml')
+            table_row = soup.find('div', attrs={'class': 'cars-list'})
+            regex_containr = re.compile('.*(?=item-lift-container)')
+            try:
                 item_lift_container = table_row.find_all('div', attrs={'class': 'col-12 col-lg-3 item-lift-container'})
-                if item_lift_container:  # Проверка, что список не пуст
-                    file_name = 'id_ad.csv'
+            except:
+                continue
+            if item_lift_container:  # Проверка, что список не пуст
+                file_name = 'id_ad.csv'
+                try:
+                    with open(file_name, 'r', encoding='utf-8') as csvfile:
+                        reader = csv.reader(csvfile)
+                        id_list = [row[0] for row in reader]
+                except:
+                    with open(file_name, 'a', encoding='utf-8') as csvfile:
+                        reader = csv.reader(csvfile)
+                i = item_lift_container[0]  # Получение первого элемента списка
+                id_avto = i.find('div', {'class': 'item-lift'}).get('data-auction-id')
+                # print(id_avto)
+                if id_avto in id_list:
+                    continue
+                # time.sleep(1)
+                else:
+                    print(f"Новый id {id_avto}_____________________________________________________")
+                    print(url)
+                    with open(file_name, 'a', newline='', encoding='utf-8') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow([id_avto])
+                    link = 'https://www.vaurioajoneuvo.fi' + i.find('a').get('href')
+                    # Извлечь текст из блока с названием
+                    title = i.find('strong').text
+                    # Извлечь текст из блока с информацией о цене
+                    regex_price = re.compile('item-lift-price-now auction-price-now.*')
                     try:
-                        with open(file_name, 'r', encoding='utf-8') as csvfile:
-                            reader = csv.reader(csvfile)
-                            id_list = [row[0] for row in reader]
+                        price = i.find('strong', {'class': regex_price}).text
                     except:
-                        with open(file_name, 'a', encoding='utf-8') as csvfile:
-                            reader = csv.reader(csvfile)
-                    i = item_lift_container[0]  # Получение первого элемента списка
-                    id_avto = i.find('div', {'class': 'item-lift'}).get('data-auction-id')
-                    # print(id_avto)
-                    if id_avto in id_list:
-                        continue
-                    # time.sleep(1)
-                    else:
-                        print(f"Новый id {id_avto}_____________________________________________________")
-                        print(url)
-                        with open(file_name, 'a', newline='', encoding='utf-8') as csvfile:
-                            writer = csv.writer(csvfile)
-                            writer.writerow([id_avto])
-                        link = 'https://www.vaurioajoneuvo.fi' + i.find('a').get('href')
-                        # Извлечь текст из блока с названием
-                        title = i.find('strong').text
-                        # Извлечь текст из блока с информацией о цене
-                        regex_price = re.compile('item-lift-price-now auction-price-now.*')
-                        try:
-                            price = i.find('strong', {'class': regex_price}).text
-                        except:
-                            price = None
-                        details_all = [span.text for span in i.find_all('span')]
-                        details = f"{details_all[0]} {details_all[1]} {details_all[2]}"
-                        message = f"Title: {title}\nLink: {link}\nPrice: {price}\nDetails: {details}"
-                        # print(message)  # print to console
+                        price = None
+                    details_all = [span.text for span in i.find_all('span')]
+                    details = f"{details_all[0]} {details_all[1]} {details_all[2]}"
+                    message = f"Title: {title}\nLink: {link}\nPrice: {price}\nDetails: {details}"
+                    # print(message)  # print to console
 
-                        # send the same message to telegram
-                        asyncio.run(send_message(bot_token, chat_id, message))
-                # time.sleep(5)
-                # print('Паузка 5сек')
+                    # send the same message to telegram
+                    asyncio.run(send_message(bot_token, chat_id, message))
+            time.sleep(5)
+            # print('Паузка 5сек')
 
 
 process_urls_and_send_messages(bot_token, chat_id, list_urls)
