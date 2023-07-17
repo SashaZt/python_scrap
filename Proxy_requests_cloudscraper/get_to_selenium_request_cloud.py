4c57ed1d7a1fb0960c4c0c909165e23ca0e368f8
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-17 14:26:11 +0300

    17_07_2023

--- Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
+++ Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import csv
+import random
 import glob
 import re
 import requests
@@ -261,6 +261,33 @@ def get_coocies():
         f.write(html.decode('utf-8'))
     if '<title>Just a moment...</title>' not in res.text:
         print("cf challenge success")
+
+"""Все что ниже для ротации прокси"""
+
+file_path = "all_proxy.txt" #Тут все прокси которые есть
+def load_proxies(file_path):
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file if '@' in line and ':' in line]
+
+
+def get_random_proxy(proxies):
+    return random.choice(proxies)
+
+"""Эту часть использовать внутри кода"""
+proxies = load_proxies(file_path)
+proxy = get_random_proxy(proxies)
+login_password, ip_port = proxy.split('@')
+login, password = login_password.split(':')
+ip, port = ip_port.split(':')
+proxy_dict = {
+    "http": f"http://{login}:{password}@{ip}:{port}",
+    "https": f"http://{login}:{password}@{ip}:{port}"
+}
+print(proxy_dict)
+
+
+
+
 
 
 if __name__ == '__main__':
