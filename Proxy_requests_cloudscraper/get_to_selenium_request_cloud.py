afc9edfcc557f1d09da1b2d1b64dbb9cb4f0f6ed
zinchyk.a <a.zinchyk83@gmail.com>
2023-07-22 10:44:29 +0300

    Изменения в Селениум

--- Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
+++ Proxy_requests_cloudscraper/get_to_selenium_request_cloud.py
@@ -11,10 +11,12 @@ from cf_clearance import sync_cf_retry, sync_stealth
 import time
 import shutil
 import tempfile
-from selenium.webdriver.chrome.service import Service
-import undetected_chromedriver as webdriver
+# import undetected_chromedriver as webdriver
+
+
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.common.exceptions import TimeoutException
-# from selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -150,25 +152,25 @@ class ProxyExtension:
 
 
 def get_chromedriver(proxy):
-    chrome_options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
 
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument("--disable-gpu")
+    # options.add_experimental_option("excludeSwitches", ['enable-automation'])
     # chrome_options.add_argument('--disable-infobars')
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--ignore-ssl-errors')
-    # chrome_options.add_argument('--disable-extensions') # Отключает использование расширений
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-setuid-sandbox')
-    chrome_options.add_argument(
+    options.add_argument("--start-maximized")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    # options.add_argument('--disable-extensions') # Отключает использование расширений
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-setuid-sandbox')
+    options.add_argument(
             '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36')
     proxy_extension = ProxyExtension(*proxy)
-    chrome_options.add_argument(f"--load-extension={proxy_extension.directory}")
-    s = Service(executable_path="C:\\scrap_tutorial-master\\chromedriver.exe")
-    driver = webdriver.Chrome(service=s, options=chrome_options)
+    options.add_argument(f"--load-extension={proxy_extension.directory}")
+    service = ChromeService(executable_path='C:\\scrap_tutorial-master\\chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         'source': '''
             delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
