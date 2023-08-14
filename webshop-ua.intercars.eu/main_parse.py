33d998c22b8bb512641dc1482b3faa4da652e03d
zinchyk.a <a.zinchyk83@gmail.com>
2023-08-14 09:51:14 +0300

    Рабочий код

--- webshop-ua.intercars.eu/main_parse.py
+++ webshop-ua.intercars.eu/main_parse.py
@@ -1,89 +1,9 @@
 import glob
-import traceback
-import re
-import time
 import os
+import re
 import requests
-import undetected_chromedriver
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-# Для работы webdriver____________________________________________________
-# Для работы с драйвером селениум по Хром необходимо эти две строчки
-from selenium.webdriver.support.wait import WebDriverWait
-
 import csv
-
-
-def get_undetected_chromedriver(use_proxy=False, user_agent=None):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument(
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--start-maximized")
-    # chrome_options.add_argument('--headless')
-    """Проба"""
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-setuid-sandbox")
-
-    driver = undetected_chromedriver.Chrome()
-    return driver
-
-
-def save_link_all_product(url):
-    with open(f'C:\\scrap_tutorial-master\\webshop-ua.intercars.eu\\csv\\output.csv', newline='',
-              encoding='utf-8') as files:
-        csv_reader = list(csv.reader(files, delimiter=' ', quotechar='|'))
-        count_url = 0
-        bad_product = []
-        counter = 274870
-        for row in csv_reader[274870:]:
-            counter += 1
-            print(counter)
-            name_product = (','.join(row))
-            name_product_find = name_product.replace(",", " ")
-            name_file = name_product.replace(",", "_")
-            driver = get_undetected_chromedriver()
-            driver.get(url=url)
-            driver.maximize_window()
-            try:
-                find_product_wait = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, '//input[@class="ui-autocomplete-input"]')))
-                find_product = driver.find_element(By.XPATH, '//input[@class="ui-autocomplete-input"]')
-                find_product.send_keys(name_product_find)
-                find_product.send_keys(Keys.RETURN)
-
-                time.sleep(1)
-                try:
-                    driver.find_element(By.XPATH, '//div[contains(text(), "Немає результатів")]')
-                    driver.close()
-                    driver.quit()
-                except:
-                    button_img_wain = WebDriverWait(driver, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, '//div[@class="art-images margincenter"]')))
-                    button_img = driver.find_element(By.XPATH, '//div[@class="art-images margincenter"]').click()
-                    wait_img_full = WebDriverWait(driver, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, '//div[@class="swal2-container"]')))
-                    with open(f"c:\\intercars_html\\{name_file}.html", "w",
-                              encoding='utf-8') as file:
-                        file.write(driver.page_source)
-                    time.sleep(5)
-                    driver.close()
-                    driver.quit()
-            except Exception as e:
-                bad_product.append(name_product_find)
-                with open(f'C:\\scrap_tutorial-master\\webshop-ua.intercars.eu\\csv\\bad_product.csv', 'a', newline='',
-                          encoding='utf-8') as csvfile:
-                    writer = csv.writer(csvfile, delimiter='\n', quotechar='|')
-                    writer.writerow(bad_product)
-                traceback.print_exc()
-                driver.close()
-                driver.quit()
 
 
 def parsing_product():
@@ -106,15 +26,35 @@ def parsing_product():
             soup = BeautifulSoup(src, 'lxml')
 
             try:
-                name_product = soup.find('span', attrs={'class': 'active_filters_span'}).text.strip()
+                name_product = soup.find('span', attrs={'class': 'active_filters_span'}).text.replace("\n", "").strip()
             except:
                 name_product = filename_csv
-            try:
-                script_div = soup.find('a', attrs={'data-gc-onclick': 'dyn-gallery'})['data-dyngalposstring']
-            except:
-                script_div = None
+            selectors_img = [
+                ('a', {"data-gc-onclick": "dyn-gallery"}),
+                ('img', {"id": "article-image-thumb"}),
+                ('a', {"id": "btn_gallery_30"}),
+
+            ]
+            script_div_img = None
+            for tag, attributes in selectors_img:
+                try:
+                    if 'data-dyngalposstring' in attributes:
+                        script_div_img = soup.find(tag, attributes['data-dyngalposstring'])
+                    else:
+                        script_div_img = soup.find(tag, attributes)
+                    break  # Если нашли совпадение, выходим из цикла
+                except:
+                    continue  # Если не нашли, продолжаем проверку следующего селектора
+            # try:
+            #     script_div = soup.find('a', attrs={'data-gc-onclick': 'dyn-gallery'})['data-dyngalposstring']
+            # except:
+            #     script_div = soup.find('img', attrs={'id': 'article-image-thumb'})['data-dyngalposstring']
             pattern = re.compile(r"'src': '(.+?)',")
-            result = pattern.findall(script_div)
+            if script_div_img is not None:
+                result = pattern.findall(str(script_div_img))
+            else:
+                result = []
+            # print(result)
             filenames = []
             img_dir = "c:\\intercars_img"
             filenames = []
@@ -133,7 +73,6 @@ def parsing_product():
                 img_data = requests.get(img)
                 with open(file_path, 'wb') as file_img:
                     file_img.write(img_data.content)
-
 
             """Получение данных"""
             """Проверка нескольких условий"""
@@ -154,18 +93,20 @@ def parsing_product():
 
             # Если full_name остался None, значит ни одно из условий не сработало
             details_card_div = soup.find("div", {"id": "details_card"})
+            barcode = None
             try:
                 divs = details_card_div.find_all("div", class_="clearfix flexcard p-l-2 p-r-2")
+                pattern = re.compile(r"Штрих-ко.*?(\d{13})")
+
+                for div in divs:
+                    match = pattern.search(div.text)
+                    if match:
+                        barcode = match.group(1)
+                        barcode = re.sub(r"\D", "", barcode)  # Удаляем все символы, кроме цифр
+                        break
             except:
                 pass
-            pattern = re.compile(r"Штрих-ко.*?(\d{13})")
-            barcode = ''
-            for div in divs:
-                match = pattern.search(div.text)
-                if match:
-                    barcode = match.group(1)
-                    barcode = re.sub(r"\D", "", barcode)  # Удаляем все символы, кроме цифр
-                    break
+
             try:
                 manufacture = soup.find('span', attrs={'id': 'manufacture_30'}).text
             except:
@@ -182,10 +123,6 @@ def parsing_product():
             writer.writerow([full_name, manufacture, manufacture_code, manufacture_name, barcode, filenames])
 
 
-
 if __name__ == '__main__':
-    # # Собираем все ссылки на категории товаров
-    # url = "https://webshop-ua.intercars.eu/zapchasti/"
-    # save_link_all_product(url)
-    # Парсим все товары из файлов с
+    # Парсим все товары из файлов с HTML файлов
     parsing_product()
