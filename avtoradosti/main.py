01ef938d3518ec955e63a7d3bc7d175cecfbb655
zinchyk.a <a.zinchyk83@gmail.com>
2023-08-18 08:52:28 +0300

    Запись метаданных в изображение

--- avtoradosti/main.py
+++ avtoradosti/main.py
@@ -264,6 +264,24 @@ def parsin_product():
             #     print("All images are already downloaded.")
             writer.writerow([sku, title, filenames] + ordered_characteristics)
 
+def write_metadata_in_jpg():
+    """Запись метаданных в изображение"""
+    import piexif
+    image_path = "C:\\scrap_tutorial-master\\Temp\\036_BOSCH_02.jpg"
+    output_image_path = "C:\\scrap_tutorial-master\\Temp\\036_BOSCH_02_new.jpg"
+
+    # Загрузить EXIF данные из изображения
+    exif_dict = piexif.load(image_path)
+
+    # Добавить или изменить метаданные
+    exif_dict['0th'][piexif.ImageIFD.ImageDescription] = "intercars"
+    exif_dict['Exif'][piexif.ExifIFD.UserComment] = b"ASCII\0\0\0intercars"
+
+    # Конвертировать словарь обратно в байты
+    exif_bytes = piexif.dump(exif_dict)
+
+    # Записать обратно в изображение
+    piexif.insert(exif_bytes, image_path, output_image_path)
 
 if __name__ == '__main__':
     # get_requests()
