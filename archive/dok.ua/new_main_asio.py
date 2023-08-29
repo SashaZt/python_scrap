44f5249649228046509b778d4aee465c651e1d56
zinchyk.a <a.zinchyk83@gmail.com>
2023-08-29 08:38:15 +0300

    Рабочий вариант

--- archive/dok.ua/new_main_asio.py
+++ archive/dok.ua/new_main_asio.py
@@ -7,59 +7,20 @@ import ssl
 
 import random
 from proxi import proxies
+
+"""Укр"""
 from headers_cookies_aiso import headers
+
+"""Рус"""
 # from headers_cookies_aiso_rus import headers
 coun = 0
 delta = '''
-avtoshampuni
-bity
-bokorezy
-boltorezy
-buksirovochnyy_tros
-distillirovannaya_voda
-domkraty
-dozhd
-fm_modulyatory
-germetik
-gruzovye_boksy
-gubki_mochalki
-homuty
-kamery_zadnego_vida
-kanistra
-klei
-klemmi_akkumuliatora
-kleshchi
-kompressory_avtomobilnye
-kreplenie_dlya_velosipeda_na_avto
-krepleniya_dlya_lyzh_i_snoubordov
-kruglogubcy
-leyki
-manometry
-maslo_motornoe
-molotki
-motornoe_maslo_dlya_mototekhniki
-multituly
-nabor_avtomobilista
-nabor_instrumentov
-napilniki
-nasosy
-nozhi_montazhnye
-nozhnicy_po_metallu
-ochistiteli_dvigatelya_naruzhnye
-ochistiteli_karbiuratora
-ochistiteli_kondicionera
-ochistiteli_ruk
-ochistiteli_salona
-ochistiteli_tormoznoy_sistemy
-ochki_zashchitnye
-ognetushitel
-omyvatel
-otvertki
-parktroniki
-passatizhi
+akkumuliator
 '''
-# folders = 'passatizhi'
+
+
 delta_list = delta.strip().split("\n")
+"""Рабочий код"""
 async def fetch(session, url, coun, folders):
     proxy = random.choice(proxies)
     proxy_host = proxy[0]
@@ -67,10 +28,12 @@ async def fetch(session, url, coun, folders):
     proxy_user = proxy[2]
     proxy_pass = proxy[3]
     proxi = f'http://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}'
+    """Укр"""
     name_files = Path(f'c:/DATA/dok_ua/products/{folders}/ua/') / f'data_{coun}.html'
+    """Рус"""
     # name_files = Path(f'c:/DATA/dok_ua/products/{folders}/rus/') / f'data_{coun}.html'
     if not os.path.exists(name_files):
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)  # Используйте подходящую версию протокола
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)  # Используйте подходящую версию протокола
         ssl_context.verify_mode = ssl.CERT_NONE  # Отключите проверку сертификата
         try:
             async with session.get(url, headers=headers, proxy=proxi, ssl=ssl_context) as response:
@@ -83,7 +46,11 @@ async def main():
     global coun
     for folders in delta_list:
         coun = 0  # Сброс счётчика для нового значения folders
+        """Укр"""
         name_files = Path(f'c:/scrap_tutorial-master/archive/dok.ua/link/') / f'{folders}.csv'
+        """Рус"""
+        # name_files = Path(f'c:/scrap_tutorial-master/archive/dok.ua/link/') / f'{folders}_rus.csv'
+
         async with aiohttp.ClientSession() as session:
             with open(name_files, newline='', encoding='utf-8') as files:
                 urls = list(csv.reader(files, delimiter=' ', quotechar='|'))
