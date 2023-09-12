1fabb5c7c11cfc801bc6dc70c7dd79dd176f38e0
zinchyk.a <a.zinchyk83@gmail.com>
2023-09-12 14:25:27 +0300

    Рабочая

--- debank_com/main.py
+++ debank_com/main.py
@@ -1,35 +1,10 @@
-import glob
 import csv
-import datetime
+import glob
 import json
-import time
-
-import requests
-import requests
-import json
-import random
 import os
-
-file_path = "proxy.txt"
-
-
-def load_proxies(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            return [line.strip() for line in file if '@' in line and ':' in line]
-    except FileNotFoundError:
-        return []
-
-
-# def get_random_proxy(proxies):
-#     return proxies if proxies else None
-#     # return random.choice(proxies) if proxies else None
-
-
-def get_random_proxy(proxies):
-    if not hasattr(get_random_proxy, "_proxy_generator"):
-        get_random_proxy._proxy_generator = iter(proxies)
-    return next(get_random_proxy._proxy_generator)
+import time
+import sys
+import requests
 
 
 def get_wallet():
@@ -41,95 +16,36 @@ def get_wallet():
                 yield w[0], w[1]  # Используем yield для генерации кошелька и даты
 
 
-def main():
-    for wallet, date in get_wallet():  # Итерируемся по всем кошелькам и датам
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-            'DNT': '1',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'cross-site',
-        }
-
-        start_time = 0
-        while True:
-            filename = f'{wallet}_{start_time}.json'
-            # if os.path.exists(filename):  # Проверка на существование файла
-            #     print(f"File {filename} already exists. Skipping.")
-            #     continue
-            proxy_dict = None
-
-            # proxies = load_proxies(file_path)
-            # proxy = get_random_proxy(proxies)
-            proxy = 'proxy_alex:DbrnjhbZ88@141.145.205.4:31281'
-            login_password, ip_port = proxy.split('@')
-            login, password = login_password.split(':')
-            ip, port = ip_port.split(':')
-            proxy_url = f'http://{login}:{password}@{ip}:{port}'
-            proxy_dict = {'http': proxy_url, 'https': proxy_url}
-            params = {
-                'user_addr': wallet,
-                'start_time': start_time,
-                'page_count': '20',
-            }
-            response = requests.get('https://api.debank.com/history/list', proxies=proxy_dict, params=params,
-                                    headers=headers)
-            if response.status_code == 200:
-                print(f'{proxy_dict}--------------------------{response.status_code}')
-                # if os.path.exists(filename):  # Проверка на существование файла
-                #     print(f"File {filename} already exists. Skipping.")
-                #     break
-                json_data = response.json()
-
-                with open(f'{wallet}_{start_time}.json', 'w', encoding='utf-8') as f:
-                    json.dump(json_data, f, ensure_ascii=False, indent=4)  # Записываем в файл
-
-                if len(json_data['data']['history_list']) == 0:
-                    break  # выход из цикла, если history_list пуст
-
-                # Обновляем start_time на значение time_at последнего элемента
-                for j in json_data['data']['history_list'][-1:]:
-                    time_at = int(j['time_at'])
-
-                if time_at == date:
-                    break  # выход из цикла, если time_at равен date
-
-                start_time = time_at  # обновляем start_time для следующего запроса
-                time.sleep(60)
-            elif response.status_code == 429:
-                print(proxy_dict)
-                continue
-                # if os.path.exists(filename):  # Проверка на существование файла
-                #     print(f"File {filename} already exists. Skipping.")
-                #     continue
-
-    #
-    # timestamp = 1694034661
-    # dt_object = datetime.datetime.fromtimestamp(timestamp)
-    #
-    # print(dt_object)
-    #
-    # Создать объект datetime для даты 2023-08-13 00:00:00
-    # dt_object = datetime.datetime(year=2023, month=7, day=9, hour=11, minute=47, second=14)
-    #
-    # # Перевести объект datetime в Unix-время
-    # timestamp = int(dt_object.timestamp())
-    #
-    # print(timestamp)
+def get_token():
+    with open('token.txt', 'r') as file:
+        first_value = file.readline().strip()
+        return first_value
 
 
 def get_api():
+    # Определите текущую директорию, где находится скрипт
+    current_directory = os.getcwd()
+
+    # Задайте имя папки data_json
+    data_json_directory = 'data_json'
+
+    # Создайте полный путь к папке data_json
+    data_json_path = os.path.join(current_directory, data_json_directory)
+
+    # Проверьте, существует ли папка data_json, и если нет, создайте ее
+    if not os.path.exists(data_json_path):
+        os.makedirs(data_json_path)
     for wallet, date in get_wallet():  # Итерируемся по всем кошелькам и датам
+        date = int(date)
+        token = get_token()
         headers = {
             'accept': 'application/json',
-            'AccessKey': '09ac3e9d6bce2b9b97672400bc3adf7ab2c2f279',
+            'AccessKey': token,
         }
 
         start_time = 0
+        count = 0
+        print(f'Сохраняю файлы по кошельку {wallet}')
         while True:
             filename = f'{wallet}_{start_time}.json'
 
@@ -145,8 +61,18 @@ def get_api():
             response = requests.get('https://pro-openapi.debank.com/v1/user/all_history_list', params=params,
                                     headers=headers)
             json_data = response.json()
+            try:
+                first_operation = int(json_data['history_list'][:1][0]['time_at'])
+            except (IndexError, KeyError, TypeError):
+                try:
+                    first_operation = int(json_data['history_list'][1:2][0]['time_at'])
+                except (IndexError, KeyError, TypeError):
+                    first_operation = None  # или какое-либо другое значение по умолчанию
 
-            with open(f'{wallet}_{start_time}.json', 'w', encoding='utf-8') as f:
+            if first_operation is not None and first_operation == date:
+                break
+            file_path = os.path.join(data_json_path, filename)
+            with open(file_path, 'w', encoding='utf-8') as f:
                 json.dump(json_data, f, ensure_ascii=False, indent=4)  # Записываем в файл
 
             if len(json_data['history_list']) == 0:
@@ -160,88 +86,123 @@ def get_api():
                 break  # выход из цикла, если time_at равен date
 
             start_time = time_at  # обновляем start_time для следующего запроса
+            count += 1
+            print(f'Сохранил файл {count}')
+
+    print(f'Данные кошельков сохранены')
+    time.sleep(5)
 
 
 def par_json():
-    folder = r'c:\scrap_tutorial-master\debank_com\data_json\*.json'
+    # Определите текущую директорию, где находится скрипт
+    current_directory = os.getcwd()
+
+    # Задайте имя папки data_json
+    data_json_directory = 'data_json'
+
+    # Создайте полный путь к папке data_json
+    data_json_path = os.path.join(current_directory, data_json_directory)
+
+    folder = fr'{data_json_path}\*.json'
     files_html = glob.glob(folder)
-    with (open(f"result.csv", "w",
-               errors='ignore', encoding="utf-8") as file_csv):
-        writer = csv.writer(file_csv, delimiter=",")
-        writer.writerow(
-            (
-                'wallet', 'cate_id', 'cex_id', 'chain', 'id', 'is_scam', 'other_addr', 'project_id', 'cate_id',
-                'receives_amount', 'receives_from_addr', 'receives_token_id', 'sends_amount', 'sends_to_addr',
-                'sends_token_id', 'time_at', 'token_approve_spender', 'token_approve_token_id', 'token_approve_value',
-                'tx_from_eth_gas_fee', 'tx_from_addr', 'tx_message',
-                'tx_name', 'tx_selector', 'tx_status', 'tx_to_addr', 'tx_usd_gas_fee', 'tx_value'
-            ))
-        for item in files_html:
-            file_name = os.path.splitext(os.path.basename(item))[0]
-            # Разделяем имя файла по символу "_"
-            parts = file_name.split('_')
+    if not files_html:
+        sys.exit("Новых данных нету")
 
-            # Возьмем первую часть разделенного имени файла
-            wallet = parts[0]
+    with open("wallet.csv", "w", errors='ignore', newline='', encoding="utf-8") as wallet_csv:
+        wallet_writer = csv.writer(wallet_csv, delimiter=",")
+        # wallet_writer.writerow(['first_operation', 'wallet'])  # Заголовок
 
-            with open(item, 'r', encoding="utf-8") as f:
-                data_json = json.load(f)
-                for j in data_json['history_list']:
+        with open("wallet_transactions.csv", "w", errors='ignore', newline='', encoding="utf-8") as result_csv:
+            result_writer = csv.writer(result_csv, delimiter=",")
+            result_writer.writerow(
+                (
+                    'wallet', 'cate_id', 'cex_id', 'chain', 'id', 'is_scam', 'other_addr', 'project_id', 'cate_id',
+                    'receives_amount', 'receives_from_addr', 'receives_token_id', 'sends_amount', 'sends_to_addr',
+                    'sends_token_id', 'time_at', 'token_approve_spender', 'token_approve_token_id',
+                    'token_approve_value',
+                    'tx_from_eth_gas_fee', 'tx_from_addr', 'tx_message',
+                    'tx_name', 'tx_selector', 'tx_status', 'tx_to_addr', 'tx_usd_gas_fee', 'tx_value'
+                ))
 
-                    # for j in data_json['data']['history_list']:
-                    cate_id = j['cate_id']
-                    cex_id = j['cex_id']
-                    chain = j['chain']
-                    j_id = j['id']
-                    is_scam = j['is_scam']
-                    other_addr = j['other_addr']
-                    project_id = j['project_id']
+            for item in files_html:
+                file_name = os.path.splitext(os.path.basename(item))[0]
+                parts = file_name.split('_')
+                wallet = parts[0]
 
-                    receives_data = j.get("receives", [])
-                    receives_amount = receives_data[0].get('amount') if receives_data and 'amount' in receives_data[
-                        0] else None
-                    receives_from_addr = receives_data[0].get('from_addr') if receives_data and 'from_addr' in \
-                                                                              receives_data[0] else None
-                    receives_token_id = receives_data[0].get('token_id') if receives_data and 'token_id' in \
-                                                                            receives_data[0] else None
+                with open(item, 'r', encoding="utf-8") as f:
+                    data_json = json.load(f)
+                    if '_0' in file_name:
+                        first_operation = int(data_json['history_list'][:1][0]['time_at'])
+                        # Записать данные в wallet.csv
+                        wallet_writer.writerow([wallet, first_operation])
 
-                    sends_data = j.get("sends", [])
-                    sends_amount = sends_data[0].get('amount') if sends_data and 'amount' in sends_data[0] else None
-                    sends_price = sends_data[0].get('price') if sends_data and 'price' in sends_data[0] else None
-                    sends_to_addr = sends_data[0].get('to_addr') if sends_data and 'to_addr' in sends_data[0] else None
-                    sends_token_id = sends_data[0].get('token_id') if sends_data and 'token_id' in sends_data[
-                        0] else None
+                    for j in data_json['history_list']:
+                        cate_id = j['cate_id']
+                        cex_id = j['cex_id']
+                        chain = j['chain']
+                        j_id = j['id']
+                        is_scam = j['is_scam']
+                        other_addr = j['other_addr']
+                        project_id = j['project_id']
 
-                    time_at = int(j['time_at'])
-                    token_approve_data = j.get("token_approve", [])
-                    token_approve_spender = token_approve_data.get('spender') if token_approve_data and 'spender' in \
-                                                                                    token_approve_data else None
-                    token_approve_token_id = token_approve_data.get(
-                        'token_id') if token_approve_data and 'token_id' in token_approve_data else None
-                    token_approve_value = token_approve_data.get('value') if token_approve_data and 'value' in \
-                                                                                token_approve_data else None
+                        receives_data = j.get("receives", [])
+                        receives_amount = receives_data[0].get('amount') if receives_data and 'amount' in receives_data[
+                            0] else None
+                        receives_from_addr = receives_data[0].get('from_addr') if receives_data and 'from_addr' in \
+                                                                                  receives_data[0] else None
+                        receives_token_id = receives_data[0].get('token_id') if receives_data and 'token_id' in \
+                                                                                receives_data[0] else None
 
-                    tx_data = j.get("tx", [])
-                    tx_from_eth_gas_fee = tx_data.get('eth_gas_fee') if tx_data and 'eth_gas_fee' in tx_data else None
-                    tx_from_addr = tx_data.get('from_addr') if tx_data and 'from_addr' in tx_data else None
-                    tx_message = tx_data.get('message') if tx_data and 'message' in tx_data else None
-                    tx_name = tx_data.get('name') if tx_data and 'name' in tx_data else None
-                    tx_selector = tx_data.get('selector') if tx_data and 'selector' in tx_data else None
-                    tx_status = tx_data.get('status') if tx_data and 'status' in tx_data else None
-                    tx_to_addr = tx_data.get('to_addr') if tx_data and 'to_addr' in tx_data else None
-                    tx_usd_gas_fee = tx_data.get('usd_gas_fee') if tx_data and 'usd_gas_fee' in tx_data else None
-                    tx_value = tx_data.get('value') if tx_data and 'value' in tx_data else None
+                        sends_data = j.get("sends", [])
+                        sends_amount = sends_data[0].get('amount') if sends_data and 'amount' in sends_data[0] else None
+                        sends_price = sends_data[0].get('price') if sends_data and 'price' in sends_data[0] else None
+                        sends_to_addr = sends_data[0].get('to_addr') if sends_data and 'to_addr' in sends_data[
+                            0] else None
+                        sends_token_id = sends_data[0].get('token_id') if sends_data and 'token_id' in sends_data[
+                            0] else None
 
-                    datas = [wallet, cate_id, cex_id, chain, j_id, is_scam, other_addr, project_id, cate_id,
-                             receives_amount, receives_from_addr, receives_token_id, sends_amount,
-                             sends_to_addr, sends_token_id, time_at, token_approve_spender, token_approve_token_id,
-                             token_approve_value, tx_from_eth_gas_fee, tx_from_addr,
-                             tx_message, tx_name, tx_selector, tx_status, tx_to_addr, tx_usd_gas_fee, tx_value]
-                    writer.writerow(datas)
+                        time_at = int(j['time_at'])
+                        token_approve_data = j.get("token_approve", [])
+                        token_approve_spender = token_approve_data.get(
+                            'spender') if token_approve_data and 'spender' in token_approve_data else None
+                        token_approve_token_id = token_approve_data.get(
+                            'token_id') if token_approve_data and 'token_id' in token_approve_data else None
+                        token_approve_value = token_approve_data.get(
+                            'value') if token_approve_data and 'value' in token_approve_data else None
+
+                        tx_data = j.get("tx", [])
+                        tx_from_eth_gas_fee = tx_data.get(
+                            'eth_gas_fee') if tx_data and 'eth_gas_fee' in tx_data else None
+                        tx_from_addr = tx_data.get('from_addr') if tx_data and 'from_addr' in tx_data else None
+                        tx_message = tx_data.get('message') if tx_data and 'message' in tx_data else None
+                        tx_name = tx_data.get('name') if tx_data and 'name' in tx_data else None
+                        tx_selector = tx_data.get('selector') if tx_data and 'selector' in tx_data else None
+                        tx_status = tx_data.get('status') if tx_data and 'status' in tx_data else None
+                        tx_to_addr = tx_data.get('to_addr') if tx_data and 'to_addr' in tx_data else None
+                        tx_usd_gas_fee = tx_data.get('usd_gas_fee') if tx_data and 'usd_gas_fee' in tx_data else None
+                        tx_value = tx_data.get('value') if tx_data and 'value' in tx_data else None
+
+                        # Записать данные в result.csv
+                        datas = [wallet, cate_id, cex_id, chain, j_id, is_scam, other_addr, project_id, cate_id,
+                                 receives_amount, receives_from_addr, receives_token_id, sends_amount,
+                                 sends_to_addr, sends_token_id, time_at, token_approve_spender, token_approve_token_id,
+                                 token_approve_value, tx_from_eth_gas_fee, tx_from_addr,
+                                 tx_message, tx_name, tx_selector, tx_status, tx_to_addr, tx_usd_gas_fee, tx_value]
+                        result_writer.writerow(datas)
+
+    # Получаем список всех файлов в папке
+    files = glob.glob(os.path.join(data_json_path, '*'))
+
+    # Удаляем каждый файл
+    for f in files:
+        if os.path.isfile(f):
+            os.remove(f)
+
+    print("Все удачно выполнено")
+    time.sleep(5)
+
 
 
 if __name__ == '__main__':
-    # main()
-    # get_api()
-    # get_wallet()
+    get_api()
     par_json()
