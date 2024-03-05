40bca6c69cf2f0b77aea4513d0ceea806ae1d202
zinchyk.a <a.zinchyk83@gmail.com>
2024-03-05 10:15:28 +0200

    05032024

--- manyvids/daily_sales.py
+++ manyvids/daily_sales.py
@@ -1557,7 +1557,10 @@ def get_table_04_to_google():
     sheet_payout_history.update([[current_datetime]], 'A1')
     if os.path.exists(filename):
         os.remove(filename)
+
+
 def get_asio():
+    import glob
     import asyncio
     import json
     import os
@@ -1571,7 +1574,7 @@ def get_asio():
     from playwright.async_api import TimeoutError
     from playwright.async_api import async_playwright
 
-    from config import db_config_asio, headers
+    from config import db_config_asio, headers, use_bd
     from proxi import proxies
 
     current_directory = os.getcwd()
@@ -1579,12 +1582,9 @@ def get_asio():
     # Создайте полный путь к папке temp
     temp_path = os.path.join(current_directory, temp_directory)
     cookies_path = os.path.join(temp_path, 'cookies')
-    login_pass_path = os.path.join(temp_path, 'login_pass')
     daily_sales_path = os.path.join(temp_path, 'daily_sales')
-    monthly_sales_path = os.path.join(temp_path, 'monthly_sales')
     payout_history_path = os.path.join(temp_path, 'payout_history')
     pending_custom_path = os.path.join(temp_path, 'pending_custom')
-    html_path = os.path.join(temp_path, 'html')
     chat_path = os.path.join(temp_path, 'chat')
 
     async def proxy_random():
@@ -1633,7 +1633,7 @@ def get_asio():
         filename = os.path.join(cookies_path, f"{identifier}_{mvtoken}.json")
         async with aiofiles.open(filename, 'w') as f:
             await f.write(json.dumps(cookies))
-        print(f"Сохранены куки для {identifier}")
+        # print(f"Сохранены куки для {identifier}")
         return filename
 
     async def load_cookies_and_update_session(session, filename):
@@ -1710,6 +1710,97 @@ def get_asio():
         async with aiofiles.open(filename, 'w', encoding='utf-8') as f:
             await f.write(content)
 
+    async def get_requests_chat(session, proxy, headers, mvtoken, filename):
+        data = {
+            'mvtoken': mvtoken,
+            'typeMessage': 'private',
+            'action': 'clc',
+            'isMobile': '0'
+        }
+        """Получаем дневные продажи"""
+        # Загрузка кук из файла и обновление сессии
+        await load_cookies_and_update_session(session, filename)
+
+        roxy_auth = None
+        if proxy['username'] and proxy['password']:
+            proxy_auth = BasicAuth(login=proxy['username'], password=proxy['password'])
+
+        async with session.post('https://www.manyvids.com/includes/user_messages.php',
+                                headers=headers, proxy=proxy['server'],
+                                proxy_auth=proxy_auth, data=data) as response:
+            data_json = await response.text()
+            return data_json
+
+    async def save_chat_json(data_json, mvtoken):
+        filename = os.path.join(chat_path, f'{mvtoken}.json')
+        async with aiofiles.open(filename, mode='w', encoding='utf-8') as f:
+            await f.write(json.dumps(data_json, ensure_ascii=False, indent=4))
+
+    async def get_total_msg():
+        folder = os.path.join(chat_path, '*.json')
+        files_json = glob.glob(folder)
+        total_pages = 0
+
+        for item in files_json:
+            filename = os.path.basename(item)
+            parts = filename.split("_")
+            mvtoken_cookies = parts[0]
+
+            async with aiofiles.open(item, 'r', encoding="utf-8") as f:
+                json_data = await f.read()
+                json_string_unescaped = json.loads(json_data)
+
+                # Второе преобразование: из строки JSON в объект Python (в данном случае, в словарь)
+                json_data = json.loads(json_string_unescaped)
+                # json_data = json.loads(json_data)
+
+                data_json = json_data['conversations']
+                total_msg = int(data_json['meta']['total'])
+                total_pages += (total_msg // 13) + 2
+
+        return total_pages
+
+    async def get_requests_all_chat(session, proxy, headers, mvtoken, filename, total_msg):
+        offset = 0
+        results = []  # Собираем результаты здесь
+
+        for i in range(1, total_msg):  # Исправлено на range
+            await load_cookies_and_update_session(session, filename)
+
+            proxy_auth = None
+            if proxy['username'] and proxy['password']:
+                proxy_auth = BasicAuth(login=proxy['username'], password=proxy['password'])
+
+            # Подготовка данных запроса
+            data = {
+                'mvtoken': mvtoken,
+                'typeMessage': 'private',
+                'action': 'clc' if i == 1 else 'cl',
+                'isMobile': '0'
+            }
+            if i > 1:
+                offset += 13
+                data.update({'offset': offset, 'type': 'all'})
+
+            # Выполнение запроса
+            async with session.post('https://www.manyvids.com/includes/user_messages.php',
+                                    headers=headers, proxy=proxy['server'],
+                                    proxy_auth=proxy_auth, data=data) as response:
+                data_json = await response.text()
+                results.append((data_json, i))  # Добавляем результат в список
+                # Добавляем случайную задержку от 0 до 5 секунд
+            await asyncio.sleep(random.uniform(0, 5))
+            if i == 5:  # Прерываем цикл после обработки пяти итераций
+                break
+
+        return results  # Возвращаем собранные результаты после завершения цикла
+
+    async def save_all_chat_json(data_json, mvtoken, i):
+        filename = os.path.join(chat_path, f'{mvtoken}_{i}.json')
+        async with aiofiles.open(filename, mode='w', encoding='utf-8') as f:
+            await f.write(json.dumps(data_json, ensure_ascii=False, indent=4))
+
+
     async def run(playwright):
         now = datetime.now()
         month = str(now.month)
@@ -1717,19 +1808,41 @@ def get_asio():
         proxy = await proxy_random()
         data_login_pass = await login_pass()
         for item in data_login_pass:
+            """Список логинов у которых не правильные пароли"""
+            skip_logins = [
+                'MaryRo',
+                'Real Sasha Grey',
+            ]
+
+            if item['login'] in skip_logins:
+                continue
             async with aiohttp.ClientSession() as session:
 
                 browser = await playwright.chromium.launch(headless=False, proxy=proxy)
                 context = await browser.new_context()
                 page = await context.new_page()
+                """Вход на страницу с логин и пароль"""
+                try:
+                    await page.goto('https://www.manyvids.com/Login/', wait_until='networkidle',
+                                    timeout=60000)  # 60 секунд
+                    # await page.goto('https://www.manyvids.com/Login/', wait_until='load', timeout=60000) нужно тестировать
+                except TimeoutError:
+                    print(f"Страница не загрузилась для {item['login']} 60 секунд.")
+                    continue
+                try:
+                    # Ожидаем появления элемента h1 с текстом "Login to ManyVids"
+                    await page.wait_for_selector('h1:text("Login to ManyVids")', timeout=60000)
+                    await page.fill('input#triggerUsername', item['login'])
+                    await page.fill('input#triggerPassword', item['password'])
+                except TimeoutError:
+                    print(f"Страница не загрузилась для {item['login']} 60 секунд.")
+                    continue
 
-                await page.goto('https://www.manyvids.com/Login/')
-                await page.fill('input#triggerUsername', item['login'])
-                await page.fill('input#triggerPassword', item['password'])
                 # Попытка входа и повтор в случае необходимости
-                max_attempts = 5  # Максимальное количество попыток
+                max_attempts = 3  # Максимальное количество попыток
                 attempts = 0  # Счетчик попыток
 
+                """Проверяем или кнопка login  пропала"""
                 while attempts < max_attempts:
                     await page.press('input#triggerPassword', 'Enter')
                     try:
@@ -1747,22 +1860,54 @@ def get_asio():
 
                 if attempts >= max_attempts:
                     print(f"Проверьте логин и пароль {item['login']}")
-                await page.goto('https://www.manyvids.com/View-my-earnings/')
-                await asyncio.sleep(10)
+
+                """Переход на страницу с данными"""
+                try:
+                    await page.goto('https://www.manyvids.com/View-my-earnings/', wait_until='networkidle',
+                                    timeout=60000)  # 60 секунд
+                    # await page.goto('https://www.manyvids.com/Login/', wait_until='load', timeout=60000) нужно тестировать
+                except TimeoutError:
+                    print(f"Страница не загрузилась для {item['login']} 60 секунд.")
+                    continue
+                try:
+                    # Ожидаем элемент в течение 30 секунд
+                    element = await page.wait_for_selector('//div[@class="text-left"]', timeout=60000)
+                    # Если элемент найден, останавливаем загрузку страницы
+                    # if element:
+                    #     await page.evaluate("window.stop()")
+                except TimeoutError:
+                    print(f"Страница не загрузилась для {item['login']} 60 секунд.")
+                    continue
 
                 await page.wait_for_selector('[data-mvtoken]')
                 mvtoken = await page.get_attribute('[data-mvtoken]', 'data-mvtoken')  # Используйте await здесь
                 filename = await save_cookies(page, item['identifier'], mvtoken)
 
                 await load_cookies_and_update_session(session, filename)  # Загружаем куки в session
-
+                """Дневные продажи"""
                 data_json_day = await get_requests_day(session, proxy, headers, mvtoken, month, filterYear, filename)
                 await save_day_json(data_json_day, mvtoken, month, filterYear)
-
+                """История"""
                 data_json_history = await get_requests_history(session, proxy, headers, mvtoken, filterYear, filename)
                 await save_history_json(data_json_history, mvtoken, filterYear)
-
+                """Pending"""
                 await save_page_content(page, mvtoken, filterYear)
+                #
+                """Загрузка чатов"""
+                data_json_first_chat = await get_requests_chat(session, proxy, headers, mvtoken, filename)
+                #
+                await save_chat_json(data_json_first_chat, mvtoken)
+
+                total_msg = await get_total_msg()
+                # data_json_all_chat, i = await get_requests_all_chat(session, proxy, headers, mvtoken, filename, total_msg)
+
+                results = await get_requests_all_chat(session, proxy, headers, mvtoken, filename, total_msg)
+                for data_json, i in results:
+                    await save_all_chat_json(data_json, mvtoken, i)
+
+                # latest_date = await check_chat()
+
+                """Закрываем"""
                 await browser.close()
 
     async def main():
@@ -1788,7 +1933,7 @@ def job():
     get_asio()
     get_sql_data_day()
     get_sql_payout_history()
-    # get_sql_chat()
+    get_sql_chat()
     # #
     get_table_01_to_google()
     #
